8c5b936785de7e03453cd19e96746ee9fdc8fe22
SufficientlySmooth <83457476+SufficientlySmooth@users.noreply.github.com>
2022-11-19 13:34:07 +0100

    Stand 19.11.2022

--- Gaußsche Strahlenoptik/Auswertung/Messdaten Errorfunktion/Erf_plot.py
+++ Gaußsche Strahlenoptik/Auswertung/Messdaten Errorfunktion/Erf_plot.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 19 11:47:29 2022
+
+@author: Jan-Philipp
+"""
 import os    
 import numpy as np 
 import matplotlib.pyplot as plt 
@@ -16,7 +22,7 @@ def plot(FILENAME):
     PATH = FILENAME+'.csv'
     
     X,Xerr,Y,Yerr = np.loadtxt(PATH,delimiter=';',skiprows=1,unpack=True)
-        
+    Y-=min(Y)
     Yerr = np.sqrt(Y)*.1
     xlabel = r'$\frac{x}{\mathrm{mm}}$' 
     ylabel = r'$\frac{U}{\mathrm{V}}$'
@@ -24,7 +30,7 @@ def plot(FILENAME):
     xlim = (-.5+min(X),max(X)+.5)
     ylim = (0,max(Y)*1.1)
 
-    popt,pcov = curve_fit(Regr,X,Y,p0=(max(Y)/2,np.mean(X),.5),maxfev=100000)
+    popt,pcov = curve_fit(Regr,X,Y,p0=(max(Y)/2,np.mean(X),1),maxfev=1000000)
     stdDev=np.sqrt(np.diag(pcov))  
         
     t1 = np.linspace(xlim[0],xlim[1], 10**4)
@@ -32,7 +38,7 @@ def plot(FILENAME):
     fig,ax=plt.subplots(1,1,figsize=(10,10/np.sqrt(2))) 
 
     ax.plot(t1,Regr(t1,*popt), marker = 'None', linestyle = '-',label=r'$U(x)=U_0\cdot\left(\mathrm{erf}\left(\frac{\sqrt{2}}{w}(x_0-x)\right)+1\right)$') 
-    ax.errorbar(X,Y,yerr=Yerr,marker='x',linestyle='None',label='Messwerte gem. \emph{'+PATH+'}')
+    ax.errorbar(X,Y,yerr=Yerr,xerr=Xerr,marker='x',linestyle='None',label='Messwerte gem. \emph{'+PATH+'}')
     for comb1, comb2 in combinations(product([-1,1],repeat=len(stdDev)),2):
         p1 = popt+stdDev*comb1
         p2 = popt+stdDev*comb2
@@ -48,16 +54,12 @@ def plot(FILENAME):
     cell_text = [[str(round(popt[i],decimals[i])),str(round(stdDev[i],decimals[i]))] for i in range(0,len(popt))]
     the_table = the_table = plt.table(cellText=cell_text,
                       rowLabels=[r'$U_0[\mathrm{V}]$',r'$w[\mathrm mm]$',r'$x_0[\mathrm mm]$'],
-                      #rowColours=colors,
                       colLabels=['Wert','Unsicherheit'],
                       loc='bottom',
                       cellLoc='center',
                       bbox=[0.25,-0.3,0.5,0.2],
-                      #AXESPAD = 0.1,
                       edges='closed')
-    #ax.bbox.get_points()
 
-    #-----Formatting------------
     ax.set_ylim(ylim[0],ylim[1])
     ax.set_xlabel(xlabel,size=16)
     ax.set_ylabel(ylabel,size=16)
@@ -66,15 +68,33 @@ def plot(FILENAME):
     ax.legend(loc='best',fontsize=15)
     fig.tight_layout()
 
-    #----Save Figure-----------
     plt.savefig(FILENAME+".pdf",dpi=1200)
     plt.savefig(FILENAME+".png",dpi=1200)
     
+    return popt[1],stdDev[1]
+    
 
 if __name__ == "__main__":    
+    w_coll = []
+    dw_coll = []
+    w_div = []
+    dw_div = []
+    z_coll = []
+    z_div = []
     path = os.getcwd() + "\\"
     dirs = os.listdir( path )
     for item in dirs:
-        if os.path.isfile(path+item) and item[::-1][:3][::-1]=='csv':
+        if os.path.isfile(path+item) and item[::-1][:3][::-1]=='csv' and item[0] in ['C','D']:
             f, e = os.path.splitext(item)
-            plot(f)
+            w,dw = plot(f)
+            z = float(f.split('_')[-1])
+            if item[0]=='C':
+                w_coll.append(w)
+                dw_coll.append(dw)
+                z_coll.append(z)
+            elif item[0]=='D':
+                w_div.append(w)
+                dw_div.append(dw)
+                z_div.append(z)
+    np.savetxt('w_z_data_coll.csv',[z_coll,w_coll,dw_coll],delimiter=',')
+    np.savetxt('w_z_data_div.csv',[z_div,w_div,dw_div],delimiter=',')
---
+++ Gaußsche Strahlenoptik/Auswertung/Messdaten Errorfunktion/w_z_plot_coll.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 18 23:08:36 2022
+
+@author: Jan-Philipp
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt 
+from scipy.optimize import curve_fit 
+import matplotlib
+from itertools import product, combinations
+from scipy import odr
+
+matplotlib.style.use('JaPh') 
+plt.ioff()
+
+def Regr(z,w0,z0):
+    lamb = .0006328
+    zR = np.pi*w0**2/lamb#*1/M**2
+    return w0*np.sqrt(1+(z-z0)**2/zR**2)
+
+def Regr_sp(tup,z):
+    return Regr(z,*tup)
+    
+def plot(CSVNAME):
+    PATH = CSVNAME + '.csv'
+
+
+    p0 = (.1,100)
+
+    X, Y, Yerr = np.loadtxt(PATH,delimiter=',')    
+
+    Xerr = np.ones(len(X))*10
+
+    xlabel = r'$\frac{z}{\mathrm{mm}}$' 
+    ylabel = r'$\frac{w(z)}{\mathrm{mm}}$'
+    
+    xlim = (min(X)-5,max(X)+5)
+    ylim = (min(Y)*0.9-.1,max(Y)*1.1)
+
+    popt,pcov = curve_fit(Regr,X,Y,p0=p0,maxfev=100000,method='lm',sigma=Yerr,absolute_sigma=True)
+    stdDev=np.sqrt(np.diag(pcov))  
+    
+
+    #----------------------------------------------------------------------
+    FuncModel = odr.Model(Regr_sp)
+    data = odr.RealData(X,Y,sx=Xerr,sy=Yerr)
+    
+    sodr = odr.ODR(data, FuncModel, beta0=list(p0),maxit=100000) # beta0 contains initial parameter values
+    regr = sodr.run() #fitting a linear curve, popt contains optimal values for the parameters a and b, pcov contains covariance of popt
+    
+    popt = regr.beta
+    pcov = regr.cov_beta 
+    stdDev=np.sqrt(np.diag(pcov))  
+    #----------------------------------------------------------------------
+
+
+    t1 = np.linspace(xlim[0],xlim[1], 10**4)
+
+    fig,ax=plt.subplots(1,1,figsize=(10,10/np.sqrt(2))) 
+  
+    ax.plot(t1,Regr(t1,*popt), marker = 'None', linestyle = '-',label=r'$w(z)=w_0\cdot\sqrt{1+\frac{(z-z_0)^2}{z_R(w_0,\lambda)^2}}$') 
+    ax.errorbar(X,Y,yerr=Yerr,xerr=Xerr,marker='x',linestyle='None',label='Messwerte')
+    for comb1, comb2 in combinations(product([-1,1],repeat=len(stdDev)),2):
+        p1 = popt+stdDev*comb1
+        p2 = popt+stdDev*comb2
+        if np.abs(np.sum(comb1))==len(stdDev) and np.sum(comb2)==-np.sum(comb1):
+            ax.fill_between(t1,Regr(t1,*p1),Regr(t1,*p2), alpha = .6, label = r'$1\sigma-\text{Fehlerstreifen}$')
+        else: 
+            ax.fill_between(t1,Regr(t1,*p1),Regr(t1,*p2), alpha = .6)
+    
+    sigfig = 4
+    stdDev_rounded = ['{:g}'.format(float('{:.{p}g}'.format(stdDev[i], p=sigfig))) for i in range(0,len(popt))]
+    decimals = [len(str(stdDev_rounded[i].split('.')[-1])) for i in range(0,len(popt))]
+    cell_text = [[str(round(popt[i],decimals[i])),str(round(stdDev[i],decimals[i]))] for i in range(0,len(popt))]
+    
+    the_table = the_table = plt.table(cellText=cell_text,
+                      rowLabels=[r'$w_0[\mathrm{mm}]$',r'$z_0[\mathrm{mm}]$'],
+                      colLabels=['Wert','Unsicherheit'],
+                      loc='bottom',
+                      cellLoc='center',
+                      bbox=[0.25,-0.3,0.5,0.2],
+                      edges='closed')
+
+    ax.set_ylim(ylim[0],ylim[1])
+    ax.set_xlabel(xlabel,size=16)
+    ax.set_ylabel(ylabel,size=16)
+    ax.set_xlim(xlim[0],xlim[1])
+    ax.grid(visible=True, which='minor', color="grey", linestyle='-',linewidth=.008, alpha=.2)
+
+    ax.legend(loc='best',fontsize=15)
+    fig.tight_layout()
+
+    #----Save Figure-----------
+    plt.savefig(CSVNAME+".pdf",dpi=1200)
+    plt.savefig(CSVNAME+".png",dpi=1200)
+
+
+
+if __name__ == "__main__":    
+   for path in ['w_z_data_coll']:
+       plot(path)
